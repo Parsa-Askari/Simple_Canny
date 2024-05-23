244c9f9d1b5dee56209be5073b650ca35dc66fe1
UNCLE-KIZARU <parsaaskari1380@gmail.com>
2024-05-23 18:50:02 +0330

    added functions

--- build/CMakeFiles/Canny.dir/main.cpp.o.d
+++ build/CMakeFiles/Canny.dir/main.cpp.o.d
@@ -348,4 +348,5 @@ CMakeFiles/Canny.dir/main.cpp.o: \
  /usr/include/opencv4/opencv2/stitching/detail/camera.hpp \
  /usr/include/opencv4/opencv2/video.hpp \
  /usr/include/opencv4/opencv2/video/tracking.hpp \
- /usr/include/opencv4/opencv2/video/background_segm.hpp
+ /usr/include/opencv4/opencv2/video/background_segm.hpp \
+ /home/parsa/Codes/opencv\ projects/c++/./imports/functions.hpp
